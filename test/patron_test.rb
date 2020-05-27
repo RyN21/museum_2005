d3012610b13c8728515a5b46de61549435ee7dfd
Ryan Laleh <laleh21@yahoo.com>
2020-05-27 09:16:16 -0600

    Add test for patron class, it exists and has attributes

--- test/patron_test.rb
+++ test/patron_test.rb
@@ -10,5 +10,8 @@ class PatronTest < Minitest::Test
 
   def test__it_exists_and_has_attributes
     assert_instance_of Patron, @patron_1
+    assert_equal "Bob", @patron_1.name
+    assert_equal 20, @patron_1.spending_money
+    assert_equal [], @patron_1.interests
   end
 end
