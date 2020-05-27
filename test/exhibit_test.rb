cad0786d13591e4b693c283d90ad53e1ffc76dac
Ryan Laleh <laleh21@yahoo.com>
2020-05-27 09:09:17 -0600

    Add test, does it exist and has attributes

--- test/exhibit_test.rb
+++ test/exhibit_test.rb
@@ -2,6 +2,18 @@ require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/sales_engine'
 require './lib/exhibit'
+require './lib/patron'
 
 
 class ExhibitTest < Minitest::Test
+  def setup
+    @exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
+  end
+
+  def test_it_exists_and_has_attribute
+    assert_instance_of Exhibit, @exhibit
+    assert_equal "Gems and Minerals", @exhibit.name
+    assert_equal 0, @exhibit.cost
+  end
+
+end
