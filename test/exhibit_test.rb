9b19ae9a4138b383f1db1589515a33a58b0cd277
Ryan Laleh <laleh21@yahoo.com>
2020-05-27 09:06:24 -0600

    Add test files, exhibit, museum, patron

---
+++ test/exhibit_test.rb
@@ -0,0 +1,7 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/sales_engine'
+require './lib/exhibit'
+
+
+class ExhibitTest < Minitest::Test
---
+++ test/museum_test.rb
@@ -0,0 +1,7 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/sales_engine'
+require './lib/museum'
+
+
+class MuseumTest < Minitest::Test
