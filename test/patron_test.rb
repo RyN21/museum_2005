2bfcfa9cfe7fc2704e597516a793abdf08ec77e8
Ryan Laleh <laleh21@yahoo.com>
2020-05-27 09:14:53 -0600

    Made exhibit class in wrong file. Fixed

--- test/patron_test.rb
+++ test/patron_test.rb
@@ -4,4 +4,11 @@ require './lib/patron'
 
 
 class PatronTest < Minitest::Test
+  def setup
+    @patron_1 = Patron.new("Bob", 20)
+  end
+
+  def test__it_exists_and_has_attributes
+    assert_instance_of Patron, @patron_1
+  end
 end
