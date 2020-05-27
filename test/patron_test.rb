319a3335affc3cb75dab52d01f8771a3c2951bd0
Ryan Laleh <laleh21@yahoo.com>
2020-05-27 09:20:15 -0600

    Add can it add interests test

--- test/patron_test.rb
+++ test/patron_test.rb
@@ -8,10 +8,14 @@ class PatronTest < Minitest::Test
     @patron_1 = Patron.new("Bob", 20)
   end
 
-  def test__it_exists_and_has_attributes
+  def test_it_exists_and_has_attributes
     assert_instance_of Patron, @patron_1
     assert_equal "Bob", @patron_1.name
     assert_equal 20, @patron_1.spending_money
     assert_equal [], @patron_1.interests
+  end
+
+  def test_it_adds_interests
+    assert_equal ["Dead Sea Scrolls", "Gems and Minerals"], @patron_1.interests
   end
 end
